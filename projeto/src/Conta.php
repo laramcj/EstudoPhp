6bf41b16f23137d7869533c4bd8c8562306d2ed7
laramcj <lara.mcj@gmail.com>
2021-09-03 09:29:13 -0300

    novo projeto Orientação a Objeto

---
+++ projeto/src/Conta.php
@@ -0,0 +1,64 @@
+<?php
+
+class Conta
+{
+    private $cpfTitular;
+    private $nomeTitular;
+    private $saldo = 0;
+
+    public function sacar(float $valorASacar): void
+    {
+        if ($valorASacar > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->saldo -= $valorASacar;
+    }
+
+    public function depositar(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+            return;
+        }
+
+        $this->saldo += $valorADepositar;
+    }
+
+    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        }
+
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+    }
+}
+
+
+//No Terminal:
+//require 'src/Conta.php';
+//$primeiraConta = new Conta();
+//$primeiraConta->saldo = 200;
+//$primeiraConta->cpfTitular = '123.456.789-10';
+//$primeiraConta->nomeTitular = 'Vinicius Dias';
+//var_dump($primeiraConta); // Verifique que o objeto já possui os dados necessários de uma conta criada
+//$segundaConta = new Conta();
+//$segundaConta->cpfTitular = '987.654.321-10';
+//$segundaConta->nomeTitular = 'Patricia';
+//$segundaConta->saldo = 1500;
+//var_dump($primeiraConta); // Verifique que o objeto não teve seus valores alterados
+//var_dump($segundaConta); // Note que este objeto possui os valores da segunda conta criada
+
+//$a = 4;
+//$b = $a;
+//$b++;
+//echo $b;
+//echo $a;
+
+//$c = $segundaConta;
+//$c->saldo = 2000;
+//var_dump($segundaConta);
