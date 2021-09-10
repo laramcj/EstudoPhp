affa481014b84e3a21210e64c5cf9dc123a898ee
laramcj <lara.mcj@gmail.com>
2021-09-10 11:09:44 -0300

    nova classe pessoa herança

--- projeto/src/Titular.php
+++ projeto/src/Titular.php
@@ -1,9 +1,7 @@
 <?php
 
-class Titular
+class Titular extends Pessoa
 {
-    private Cpf $cpf;
-    private String $nome;
     private Endereco $endereco;
 
     /**
@@ -11,32 +9,14 @@ class Titular
      */
     public function __construct(Cpf $cpf, string $nome, Endereco $endereco)
     {
-        $this->cpf = $cpf;
-        $this->validaNomeTitular($nome);
-        $this->nome = $nome;
+        parent::__construct($nome, $cpf);
         $this->endereco = $endereco;
     }
 
     /**
      * atalho interessante do PhpStorm é o "Alt + Insert", que nos dá acesso à opção "Getters"
      */
-    public function recuperaCpf(): string
-    {
-        return $this->cpf->recuperaNumero();
-    }
 
-    public function recuperaNome(): string
-    {
-        return $this->nome;
-    }
-
-    private function validaNomeTitular(string $nome)
-    {
-        if (strlen($nome) < 5) {
-            echo "Nome precisa ter pelo menos 5 caracteres";
-            exit();
-        }
-    }
 
     public function recuperaEndereco() : Endereco
     {
