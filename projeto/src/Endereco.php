dee4e6f905e3d3797620ff043d8cd60ba2c3ad6f
laramcj <lara.mcj@gmail.com>
2021-09-10 21:50:14 -0300

    namespace inicio

--- projeto/src/Endereco.php
+++ projeto/src/Endereco.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Alura\Banco\Modelo;
+
 class Endereco
 {
     private string $cidade;
@@ -48,5 +50,5 @@ class Endereco
 
 
 
-    
+
 }
--- projeto/src/Modelo/Conta/Conta.php
+++ projeto/src/Modelo/Conta/Conta.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace Alura\Banco\Modelo\Conta;
+
+
 class Conta
 {
     private $titular;
