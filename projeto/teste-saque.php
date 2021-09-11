0a3d9a0ca289e67afb1f2302b7c36ab15ae20e3f
laramcj <lara.mcj@gmail.com>
2021-09-10 23:06:38 -0300

    implementação de tarifa

---
+++ projeto/teste-saque.php
@@ -0,0 +1,19 @@
+<?php
+
+use Alura\Banco\Modelo\Conta\Conta;
+use Alura\Banco\Modelo\Conta\Titular;
+use Alura\Banco\Modelo\Cpf;
+
+require_once 'autoload.php';
+
+$conta = new Conta(
+    new Titular(
+        new Cpf('123.456.789-10'),
+        'Vinicius Dias',
+        new \Alura\Banco\Modelo\Endereco('Petropolis', 'bairro Teste', 'Rua lá', '37')
+)
+);
+$conta->deposita(500);
+$conta->saca(100);
+
+echo $conta->recuperaSaldo();
