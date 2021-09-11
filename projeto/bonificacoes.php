4beaacf6ec8627f0ad1873402a284fac34445204
laramcj <lara.mcj@gmail.com>
2021-09-11 20:03:17 -0300

    Método e classe abstratos

--- projeto/bonificacoes.php
+++ projeto/bonificacoes.php
@@ -4,13 +4,12 @@ require_once 'autoload.php';
 
 use Alura\Banco\Service\ControladorDeBonificacoes;
 use Alura\Banco\Modelo\Cpf;
-use Alura\Banco\Modelo\Funcionario\{Diretor, Gerente, Desenvolvedor};
+use Alura\Banco\Modelo\Funcionario\{Diretor, Gerente, Desenvolvedor, EditorVideo};
 
 
 $umFuncionario = new Desenvolvedor(
     'Vinicius Dias',
     new Cpf('123.456.789-10'),
-    'Desenvolvedor',
     1000
 );
 $umFuncionario->sobeDeNivel();
@@ -18,21 +17,27 @@ $umFuncionario->sobeDeNivel();
 $umaFuncionaria = new Gerente(
     'Patricia',
     new Cpf('987.654.321-10'),
-    'Gerente',
-    3000
+     3000
 );
 
 $umDiretor = new \Alura\Banco\Modelo\Funcionario\Diretor(
     'Ana Paula',
     new Cpf('123.654.987-10'),
-    'Diretora',
     5000
+);
+
+$umEditor = new EditorVideo(
+    'Paulo',
+    new Cpf('456.987.231-11'),
+    1500
 );
 
 $controlador = new ControladorDeBonificacoes();
 $controlador->adicionaBonificacaoDe($umFuncionario);
 $controlador->adicionaBonificacaoDe($umaFuncionaria);
 $controlador->adicionaBonificacaoDe($umDiretor);
+$controlador->adicionaBonificacaoDe($umEditor);
+
 
 echo $controlador->recuperaTotal();
 
