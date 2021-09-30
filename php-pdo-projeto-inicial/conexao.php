8afd7fab79ce816b16a448afb8961f22f5b22ea7
laramcj <lara.mcj@gmail.com>
2021-09-30 10:26:01 -0300

    conexão

--- php-pdo-projeto-inicial/conexao.php
+++ php-pdo-projeto-inicial/conexao.php
@@ -4,3 +4,5 @@ $caminhoBanco = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $caminhoBanco);
 
 echo 'Conectei';
+
+$pdo->exec('CREATE TABLE students (id INTEGER PRIMARY KEY, namne TEXT, birth_date TEXT );');
